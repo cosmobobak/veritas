f0c99deb74aaad746107eb17fb879a2f1afc601e
cosmobobak <cosmobobak@gmail.com>
2024-02-11 17:04:51 +0000

    use relative path to kyanite, add include script

---
+++ stronsay-exports.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export LIBRARY_PATH=$LIBRARY_PATH:/opt/cuda-12.2.0/include
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cuda-12.2.0/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cuda-12.2.0/include
+export LIBRARY_PATH=/opt/cuda-12.2.0/lib64
+export CUDNN_PATH=~/cudnn-linux-x86_64-8.9.5.30_cuda12-archive/
+export CUDA_PATH=/opt/cuda-12.2.0/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cuda-12.2.0/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/cudnn-linux-x86_64-8.9.5.30_cuda12-archive/lib
